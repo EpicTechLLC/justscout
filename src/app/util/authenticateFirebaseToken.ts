ee54b2e88954765704559559d204278310202d05
Oscar Campos-Soto <ocampossoto1@gmail.com>
2024-02-18 16:20:21 -0600

    Fixing build error.

--- src/app/util/authenticateFirebaseToken.ts
+++ src/app/util/authenticateFirebaseToken.ts
@@ -1,19 +1,20 @@
 import admin, { ServiceAccount } from "firebase-admin";
 import { DecodedIdToken } from "firebase-admin/auth";
 import { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
+export const dynamic = "force-dynamic";
 
-if (admin.apps.length === 0) {
-  admin.initializeApp({
-    credential: admin.credential.cert(
-      JSON.parse(process.env.SERVICE_ACCOUNT as string)
-    ),
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  });
-}
 export default async function authenticateFirebaseToken(
   headers: ReadonlyHeaders
 ) {
+  if (admin.apps.length === 0) {
+    admin.initializeApp({
+      credential: admin.credential.cert(
+        JSON.parse(process.env.SERVICE_ACCOUNT as string)
+      ),
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    });
+  }
   let idToken = headers.get("authorization")?.split("Bearer ")[1] as string;
 
   if (idToken) {
