3e1823ec5297eecb88ee20aaf1ce73ef19de16ca
Oscar Campos-Soto <ocampossoto1@gmail.com>
2024-02-20 22:32:31 -0600

    Revert "testing config change"
    This reverts commit 5ca1084ab395da8f210e27809c8e87db3f1fd5af.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
-	async rewrites() {
-		return [
-			{
-				source: "/__/auth/(.*)",
-				destination: `https://${process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}/__/auth/$1`
-			},
-		]
-	},
+	reactStrictMode: false
 };
 
 export default nextConfig;
